d9b2f3eed25d3485649b37606a7e37e38397b8dd
MD Waliullah <waliullah.godev@gmail.com>
2025-11-18 15:54:34 +0600

    create landing page

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,29 @@
 import { Hero } from "@/components/modules/Home/Hero";
-
+import TopRatedDoctors from "@/components/modules/Home/TopRatedDoctors";
+import Specialities from "@/components/modules/Home/Specialties";
+import Testimonials from "@/components/modules/Home/Testimonials";
+import Steps from "@/components/modules/Home/Steps";
+import Head from "next/head";
 
 export default function Home() {
   return (
    <>
-      <Hero />
-   </>
+      <Head>
+        <title>AI-Powered Healthcare - Find Your Perfect Doctor</title>
+        <meta
+          name="description"
+          content="Discover top-rated doctors tailored to your needs with our AI-powered healthcare platform. Get personalized recommendations and book appointments effortlessly."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Hero />
+        <Specialities />
+        <TopRatedDoctors />
+        <Steps />
+        <Testimonials />
+      </main>
+    </>
   );
 }
