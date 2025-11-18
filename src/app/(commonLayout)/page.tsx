07e7ddd5fae427d73fa712a95042bdb1fcefad04
MD Waliullah <waliullah.godev@gmail.com>
2025-11-18 15:34:45 +0600

    Hero section done with AI powered

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Hero } from "@/components/modules/Home/Hero";
+
 
 export default function Home() {
   return (
    <>
-     
+      <Hero />
    </>
   );
 }
