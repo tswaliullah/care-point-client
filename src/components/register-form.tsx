6569ba7914695b15fda85aef2a4c30fc0bc68fac
MD Waliullah <waliullah.godev@gmail.com>
2025-11-18 22:33:39 +0600

    updated code with ZOD

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -1,27 +1,43 @@
-"use client"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 
-import { Field, FieldDescription, FieldGroup, FieldLabel } from "./ui/field";
 import { registerPatient } from "@/services/auth/registerPatient";
 import { useActionState } from "react";
 import { Button } from "./ui/button";
+import { Field, FieldDescription, FieldGroup, FieldLabel } from "./ui/field";
 import { Input } from "./ui/input";
 
 const RegisterForm = () => {
-const [state, formAction, isPending] = useActionState(registerPatient, null);
+  const [state, formAction, isPending] = useActionState(registerPatient, null);
+  console.log(state, "state");
 
-    console.log("state: ", state)
-
+  const getFieldError = (fieldName: string) => {
+    if (state && state.errors) {
+      const error = state.errors.find((err: any) => err.field === fieldName);
+      if (error) {
+        return error.message;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  };
   return (
     <form action={formAction}>
-        <FieldGroup>
+      <FieldGroup>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {/* Name */}
           <Field>
             <FieldLabel htmlFor="name">Full Name</FieldLabel>
             <Input id="name" name="name" type="text" placeholder="John Doe" />
-           
+            {getFieldError("name") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("name")}
+              </FieldDescription>
+            )}
           </Field>
-          {/* Address */} 
+          {/* Address */}
           <Field>
             <FieldLabel htmlFor="address">Address</FieldLabel>
             <Input
@@ -31,6 +47,11 @@ const [state, formAction, isPending] = useActionState(registerPatient, null);
               placeholder="123 Main St"
             />
 
+            {getFieldError("address") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("address")}
+              </FieldDescription>
+            )}
           </Field>
           {/* Email */}
           <Field>
@@ -42,12 +63,22 @@ const [state, formAction, isPending] = useActionState(registerPatient, null);
               placeholder="m@example.com"
             />
 
+            {getFieldError("email") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("email")}
+              </FieldDescription>
+            )}
           </Field>
           {/* Password */}
           <Field>
             <FieldLabel htmlFor="password">Password</FieldLabel>
             <Input id="password" name="password" type="password" />
 
+            {getFieldError("password") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("password")}
+              </FieldDescription>
+            )}
           </Field>
           {/* Confirm Password */}
           <Field className="md:col-span-2">
@@ -58,11 +89,16 @@ const [state, formAction, isPending] = useActionState(registerPatient, null);
               type="password"
             />
 
+            {getFieldError("confirmPassword") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("confirmPassword")}
+              </FieldDescription>
+            )}
           </Field>
         </div>
         <FieldGroup className="mt-4">
           <Field>
-            <Button className="cursor-pointer" type="submit" disabled={isPending}>
+            <Button type="submit" disabled={isPending}>
               {isPending ? "Creating Account..." : "Create Account"}
             </Button>
 
--- src/services/auth/loginUser.ts
+++ src/services/auth/loginUser.ts
@@ -1,11 +1,38 @@
-"use server";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server"
 
+import z from "zod";
 
-export const loginUser = async(_currentState : any, formData: any): Promise<any> => {
+const loginValidationZodSchema = z.object({
+    email: z.email({
+        message: "Email is required",
+    }),
+    password: z.string("Password is required").min(6, {
+        error: "Password is required and must be at least 6 characters long",
+    }).max(100, {
+        error: "Password must be at most 100 characters long",
+    }),
+});
+
+export const loginUser = async (_currentState: any, formData: any): Promise<any> => {
     try {
         const loginData = {
             email: formData.get('email'),
-            password: formData.get('password')
+            password: formData.get('password'),
+        }
+
+        const validatedFields = loginValidationZodSchema.safeParse(loginData);
+
+        if (!validatedFields.success) {
+            return {
+                success: false,
+                errors: validatedFields.error.issues.map(issue => {
+                    return {
+                        field: issue.path[0],
+                        message: issue.message,
+                    }
+                })
+            }
         }
 
         const res = await fetch("http://localhost:5000/api/v1/auth/login", {
@@ -13,14 +40,14 @@ export const loginUser = async(_currentState : any, formData: any): Promise<any>
             body: JSON.stringify(loginData),
             headers: {
                 "Content-Type": "application/json",
-            }
+            },
         }).then(res => res.json());
+
 
         return res;
 
     } catch (error) {
-        return {
-            error: "Login failed..!"
-        }
+        console.log(error);
+        return { error: "Login failed" };
     }
 }
