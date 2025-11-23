f4127db92ee701de59c2c32b7e877a510024f362
MD Waliullah <waliullah.godev@gmail.com>
2025-11-23 12:06:08 +0600

    implement cookie handling functions: setCookie, getCookie, deleteCookie

---
+++ src/lib/token-handlar.ts
@@ -0,0 +1,20 @@
+import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import { cookies } from "next/headers";
+
+
+export const setCookie = async (key: string, value: string, options: Partial<ResponseCookie>) => {
+    const cookieStore = await cookies();
+    cookieStore.set(key, value, options);
+}
+
+export const getCookie = async (key: string) => {
+    const cookieStore = await cookies();
+    return cookieStore.get(key);
+}
+
+
+
+export const deleteCookie = async (key: string) => {
+    const cookieStore = await cookies();
+    cookieStore.delete(key);
+}
