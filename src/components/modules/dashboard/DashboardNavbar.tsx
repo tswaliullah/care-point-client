23bfc8151c86a6358dc3547ac1986ccea6c88fbb
MD Waliullah <waliullah.godev@gmail.com>
2025-11-23 19:07:24 +0600

    dashboard navbar and mobile sidebar components

--- src/components/modules/dashboard/DashboardNavbar.tsx
+++ src/components/modules/dashboard/DashboardNavbar.tsx
@@ -1,8 +1,20 @@
-const DashboardNavbar = () => {
+import { getDefaultDashboardRoute } from "@/lib/auth-utils";
+import { getNavItemsByRole } from "@/lib/navItems.config";
+import { getUserInfo } from "@/services/auth/getUserInfo";
+import { UserInfo } from "@/types/user.interface";
+import DashboardNavbarContent from "./DashboardNavbarContent";
+
+const DashboardNavbar = async () => {
+  const userInfo = (await getUserInfo()) as UserInfo;
+  const navItems = getNavItemsByRole(userInfo.role);
+  const dashboardHome = getDefaultDashboardRoute(userInfo.role);
+
   return (
-    <>
-        <div>DashboardNavbar</div>
-    </>
+    <DashboardNavbarContent
+      userInfo={userInfo}
+      navItems={navItems}
+      dashboardHome={dashboardHome}
+    />
   );
 };
 
