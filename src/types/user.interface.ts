0f68671d8f75702caca2927328cdb9582b200575
MD Waliullah <waliullah.godev@gmail.com>
2025-12-09 23:31:32 +0600

    feat: Add interfaces for user, review, schedule, and specialties

--- src/types/user.interface.ts
+++ src/types/user.interface.ts
@@ -1,7 +1,18 @@
 import { UserRole } from "@/lib/auth-utils";
+import { IAdmin } from "./admin.interface";
+import { IDoctor } from "./doctor.interface";
+import { IPatient } from "./patient.interface";
 
 export interface UserInfo {
+    id: string;
     name: string;
     email: string;
     role: UserRole;
+    needPasswordChange: boolean;
+    status: "ACTIVE" | "BLOCKED" | "DELETED";
+    admin?: IAdmin;
+    patient?: IPatient;
+    doctor?: IDoctor;
+    createdAt: string;
+    updatedAt: string;
 }
